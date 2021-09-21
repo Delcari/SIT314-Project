e88f0ee6de664b5f066365d5b1b943dc1754252e
Riley Dellios <delcari@protonmail.com>
2021-09-21 19:09:37 +1000

    NEW: register form handling

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ChangeEvent } from "react";
 import {
   Button,
   ButtonGroup,
@@ -8,43 +8,82 @@ import {
   FormControl,
 } from "react-bootstrap";
 
-function Register() {
-  return (
-    <div>
-      <Container fluid="sm">
-        <h1>register</h1>
-        <Row>
-          <InputGroup className="mb-3">
-            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-            <FormControl
-              placeholder="username"
-              aria-label="username"
-              aria-describedby="basic-addon1"
-            />
-          </InputGroup>
-        </Row>
-        <Row>
-          <InputGroup className="mb-3">
-            <FormControl placeholder="password" aria-label="password" />
-          </InputGroup>
-        </Row>
-        <Row>
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder="confirm password"
-              aria-label="confirm password"
-            />
-          </InputGroup>
-        </Row>
-        <Row>
-          <ButtonGroup aria-label="Basic example">
-            <Button variant="primary">register</Button>
-            <Button variant="secondary" href="/login">login</Button>
-          </ButtonGroup>
-        </Row>
-      </Container>
-    </div>
-  );
+type Rstate = {
+  username: string;
+  password: string;
+  cpassword: string;
+};
+type Rprops = {};
+
+class Register extends React.Component<Rprops, Rstate> {
+  constructor(props: Rprops) {
+    super(props);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  state: Rstate = {
+    username: "",
+    password: "",
+    cpassword: "",
+  };
+
+  handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value } as Rstate);
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>
+        <Container fluid="sm">
+          <h1>register</h1>
+          <Row>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+              <FormControl
+                placeholder="username"
+                aria-label="username"
+                aria-describedby="basic-addon1"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleInputChange}
+              />
+            </InputGroup>
+          </Row>
+          <Row>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="password"
+                aria-label="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleInputChange}
+              />
+            </InputGroup>
+          </Row>
+          <Row>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="confirm password"
+                aria-label="confirm password"
+                name="cpassword"
+                value={this.state.cpassword}
+                onChange={this.handleInputChange}
+              />
+            </InputGroup>
+          </Row>
+          <Row>
+            <ButtonGroup aria-label="Basic example">
+              <Button variant="primary">register</Button>
+              <Button variant="secondary" href="/login">
+                login
+              </Button>
+            </ButtonGroup>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Register;
