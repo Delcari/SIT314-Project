b6a2d5d301ba779eaeb535e09dbee9846a72d620
Riley Dellios <delcari@protonmail.com>
2021-10-10 21:43:12 +1100

    ENHANCE: setup env variables

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -12,11 +12,16 @@ app.use(function(req, res, next) {
 });
 
 const port = process.env.PORT || 5001;
+const api = process.env.API
+const apiPort = process.env.API_PORT
+const brokerPort = process.env.BROKER_PORT
+const broker = process.env.BROKER
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const client = mqtt.connect("mqtt://broker.emqx.io:1883");
+const client = mqtt.connect(`mqtt://${broker}:${brokerPort}`);
 
 client.on('connect', () => {
     client.subscribe(`219191105/#`, err =>{
@@ -27,7 +32,7 @@ client.on('connect', () => {
 });
 
 const updateLight = (id, status) => {
-    axios.post(`http://localhost:5000/light/update/${id}`, {
+    axios.post(`http://${api}:${apiPort}/light/update/${id}`, {
         status: status
       })
       .then((response) => {
@@ -62,7 +67,7 @@ app.get('/light/update/:id', (req, res) => {
     const { id } = req.params;
     const topic = `219191105/${id}`;
     const command = `{"source":"web","request":"true"}`
-    
+
     client.publish(topic, command, () => {
         res.send("published new message");
     });
