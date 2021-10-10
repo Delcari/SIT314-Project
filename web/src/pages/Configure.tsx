f7ec0a0fe01e7ef00bb0712a5e8b7801d3d57ef5
Riley Dellios <delcari@protonmail.com>
2021-10-10 15:06:20 +1100

    NEW: add device handler

--- web/src/pages/Configure.tsx
+++ web/src/pages/Configure.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, FormEvent, ChangeEvent } from "react";
 import {
   Button,
   ButtonGroup,
@@ -7,48 +7,148 @@ import {
   InputGroup,
   FormControl,
 } from "react-bootstrap";
+import axios from "axios";
 
-function Configure() {
-  return (
-    <div>
-      <Container fluid="sm">
-        <h1>setup device</h1>
-        <Row>
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder="device-id"
-              aria-label="device-id"
-            />
-          </InputGroup>
-        </Row>
-        <Row>
-          <InputGroup className="mb-3">
-            <FormControl placeholder="access-key" aria-label="access-key" />
-          </InputGroup>
-        </Row>
-        <Row>
-          <InputGroup className="mb-3">
-            <FormControl placeholder="group" aria-label="group" />
-          </InputGroup>
-        </Row>
-        <Row>
-          <InputGroup className="mb-3">
-            <FormControl placeholder="building" aria-label="building" />
-          </InputGroup>
-        </Row>
-        <Row>
-          <InputGroup className="mb-3">
-            <FormControl placeholder="room" aria-label="room" />
-          </InputGroup>
-        </Row>
-        <Row>
-          <ButtonGroup aria-label="Basic example">
-            <Button variant="primary">add</Button>
-          </ButtonGroup>
-        </Row>
-      </Container>
-    </div>
-  );
+type cProps = {};
+
+type cState = {
+  id: string;
+  group: string;
+  building: string;
+  room: string;
+  users: string[];
+  user: string;
+};
+
+class Configure extends React.Component<cProps, cState> {
+  constructor(props: cProps) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.addUser = this.addUser.bind(this);
+  }
+
+  state: cState = {
+    id: "",
+    group: "",
+    building: "",
+    room: "",
+    users: [],
+    user: ""
+  };
+
+  handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value } as unknown as cState);
+  }
+
+  addUser() {
+    this.setState((prevState) => ({ users: [...prevState.users, this.state.user]}));
+  }
+
+  handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/light/add", {
+        id: this.state.id,
+        group: this.state.group,
+        building: this.state.building,
+        room: this.state.room,
+        status: "on",
+        users: this.state.users,
+        dateAdded: Date.now(),
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>
+        <Container fluid="sm">
+          <h1>setup device</h1>
+          <form onSubmit={this.handleSubmit}>
+            <Row>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="id"
+                  aria-label="id"
+                  name="id"
+                  value={this.state.id}
+                  onChange={this.handleInputChange}
+                />
+              </InputGroup>
+            </Row>
+            <Row>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="group"
+                  aria-label="group"
+                  name="group"
+                  value={this.state.group}
+                  onChange={this.handleInputChange}
+                />
+              </InputGroup>
+            </Row>
+            <Row>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="building"
+                  aria-label="building"
+                  name="building"
+                  value={this.state.building}
+                  onChange={this.handleInputChange}
+                />
+              </InputGroup>
+            </Row>
+            <Row>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="room"
+                  aria-label="room"
+                  name="room"
+                  value={this.state.room}
+                  onChange={this.handleInputChange}
+                />
+              </InputGroup>
+            </Row>
+            <Row>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="add user"
+                  aria-label="user"
+                  name="user"
+                  aria-describedby="basic-addon2"
+                  value={this.state.user}
+                  onChange={this.handleInputChange}
+                />
+                <Button variant="outline-secondary" id="button-addon2" onClick={this.addUser}>
+                  Add
+                </Button>
+              </InputGroup>
+            </Row>
+            <Row>
+              <b>Users:</b>
+              <ul>
+                {this.state.users.map((user, index) => (
+                  <li>{`${user}`}</li>
+                ))}
+              </ul>
+            </Row>
+            <Row>
+              <ButtonGroup aria-label="Basic example">
+                <Button variant="primary" type="submit" value="Submit">
+                  add
+                </Button>
+              </ButtonGroup>
+            </Row>
+          </form>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Configure;
