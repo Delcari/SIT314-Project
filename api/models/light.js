e960eab129088f6d45ddda71625b39d1b1437abd
Riley Dellios <delcari@protonmail.com>
2021-10-10 15:03:53 +1100

    UPDATE: mongoose models

--- api/models/light.js
+++ api/models/light.js
@@ -2,5 +2,11 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose.model('Light', new mongoose.Schema({ 
   id: Number, 
-  name: String
+  name: String,
+  group: String, 
+  building: String,
+  room: String,
+  status: String,
+  users: [],
+  dateAdded: String
 }));
--- api/models/user.js
+++ api/models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.model('User', new mongoose.Schema({ 
-  id: Number, 
   username: String,
   password: String
 }));
